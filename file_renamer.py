d577178a3d934173007cc443b1f87dae09ef32a2
wwang126 <wavelet.wang@gmail.com>
2018-03-23 07:20:04 -0700

    added padding

--- file_renamer.py
+++ file_renamer.py
@@ -1,9 +1,16 @@
+"""Renames files"""
 import re
 
 def main():
     """Main Program runner """
-    test_string = "01-asdfasdf01-asdfasf10adsfasd22adsf33"
-    name_list = re.findall("[0-9]+-?",test_string)
-
+    test_string = "01-asdfasdf01-asdfasf10adsfasd22adsf3344"
+    num_list = re.findall("[0-9]+", test_string)
+    print(num_list)
+    max_len = len(max(num_list, key=len))
+    print(max_len)
+    new_list = []
+    for str in num_list:
+        new_list.append(str.zfill(max_len))
+    print(new_list)
 if __name__ == '__main__':
     main()
