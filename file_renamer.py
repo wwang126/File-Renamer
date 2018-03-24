c11f4bd769682693dbe5f0d0274786b30bf79064
wwang126 <wavelet.wang@gmail.com>
2018-03-24 01:11:45 -0700

    fixed replacing for differing filenames

--- file_renamer.py
+++ file_renamer.py
@@ -5,14 +5,21 @@ import os
 def main():
     """Main Program runner """
     num_list = []
+    end_list = []
+    match_list = []
     for filename in os.listdir(os.getcwd()):
         print(filename)
         num_match = re.search("[0-9]+", filename)
         if num_match:
-            num_list.append(num_match.string)
+            match_list.append(num_match.string)
+            num_list.append(num_match.group(0))
+            end_list.append(filename[num_match.end(0):])
     max_len = len(max(num_list, key=len))
     print(max_len)
-    for num in num_list:
-        os.rename(num, num.zfill(max_len))
+    idx = 0
+    for name in match_list:
+        new_name = num_list[idx].zfill(max_len) + end_list[idx]
+        os.rename(name, new_name)
+        idx += 1
 if __name__ == '__main__':
     main()
